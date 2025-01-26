80cca7f6b21d7a2a9d3618c1aa9c10316ff313a4
Sirawit-Boon <ter.sirawit1997@gmail.com>
2025-01-26 23:14:44 +0700

    feat: added navbar and hero section on landing page

---
+++ src/constants/index.ts
@@ -0,0 +1,84 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Jobs",
+    href: "#jobs",
+  },
+  {
+    id: 2,
+    name: "Companies",
+    href: "#companies",
+  },
+  {
+    id: 3,
+    name: "Login",
+    href: "#login",
+  },
+  {
+    id: 4,
+    name: "SignUp",
+    href: "#signup",
+  },
+];
+
+export const companiesLogo = [
+  {
+    id: 1,
+    name: "Google",
+    src: "/assets/google.png",
+    width: "w-30",
+  },
+  {
+    id: 2,
+    name: "Microsoft",
+    src: "/assets/microsoft.png",
+    width: "w-40",
+  },
+  {
+    id: 3,
+    name: "Amd",
+    src: "/assets/amd.png",
+    width: "w-40",
+  },
+  {
+    id: 4,
+    name: "Amazon",
+    src: "/assets/amazon.png",
+    width: "w-40",
+  },
+  {
+    id: 5,
+    name: "Agoda",
+    src: "/assets/agoda.png",
+    width: "w-30",
+  },
+]
+
+export const companiesAds = [
+  {
+    id: 1,
+    name: "Google",
+    src: "/assets/google-locate.jpeg",
+  },
+  {
+    id: 2,
+    name: "Agoda",
+    src: "/assets/agoda-locate.jpg",
+  },
+  {
+    id: 3,
+    name: "Microsoft",
+    src: "/assets/microsoft-locate.jpg",
+  },
+  {
+    id: 4,
+    name: "Amazon",
+    src: "/assets/amazon-locate.jpg",
+  },
+  // {
+  //   id: 5,
+  //   name: "Agoda",
+  //   src: "/assets/agoda.png",
+  //   width: "w-30",
+  // },
+]
---
+++ src/sections/Navbar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { navLinks } from "../constants";
+
+const isOpen = ref<boolean>(false);
+</script>
+
+<template>
+  <header>
+    <div
+      class="c-space w-full mx-auto flex justify-between bg-[#0D0F10] #0D0F10 #42b883 items-center"
+    >
+      <div class="flex justify-between gap-12">
+        <div class="flex flex-col sm:flex-row items-center gap-4">
+          <a>
+            <img src="/assets/logo.png" class="md:w-20 md:h-20 w-14 h-14" />
+          </a>
+          <h1 class="font-semibold text-xl text-[#35495e] hidden md:flex">
+            JobVavel
+          </h1>
+        </div>
+        <nav class="nav-ul text-white #35495e">
+          <a> Jobs </a>
+          <a> Companies </a>
+        </nav>
+      </div>
+      <div class="nav-ul">
+        <button class="btn">Login</button>
+        <button class="btn">Sing up</button>
+      </div>
+      <div class="sm:hidden flex">
+        <button
+          class="text-neutral-400 hover:text-white focus:outline-none"
+          @click="isOpen = !isOpen"
+        >
+          <img
+            :src="isOpen ? '/assets/close.svg' : '/assets/menu.svg'"
+            class="w-6 h-6"
+          />
+        </button>
+      </div>
+    </div>
+    <div :class="['nav-sidebar', isOpen ? 'max-h-screen' : 'max-h-0']">
+      <nav class="p-5">
+        <ul class="nav-ul_mobile">
+          <li v-for="link in navLinks" :key="link.id" class="nav-li">
+            <a :href="link.href" class="nav-li_a">{{ link.name }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
